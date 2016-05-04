18575b0e3518c251579544433af22bf0420b65f7
Qteb <qtebest.work@gmail.com>
2016-05-04 17:31:28 +0300

    improved restart algorithm

--- shell/restart-domains.sh
+++ shell/restart-domains.sh
@@ -8,8 +8,9 @@ echo "relaunching domains..."
 for domain in "${domains[@]}"
 	do
 		echo -ne "	now restarting: "$domain\\n
+        cat /dev/null > $domainspath/$domain/data/log/app.log
 		/usr/sbin/service $domain stop > /dev/null 2>&1
-		sleep 5
+		sleep 0.1
 		/usr/sbin/service $domain start > /dev/null 2>&1
 	done
 
