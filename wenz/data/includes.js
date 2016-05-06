14aba4efebb16e963b14e0b79981f454fd13f78f
Alexander Tretiak <p.seredin@proffibit.com>
2016-05-06 17:05:09 +0300

    change some scripts place

--- wenz/data/includes.js
+++ wenz/data/includes.js
@@ -28,7 +28,8 @@ module.exports = function(catalog, param, options) {
 
             bottom: [
                 '<script type="text/javascript" src="http://www.' + catalog + '.catalogi.ru/static/js/kmo.min.js"></script>',
-                '<script type="text/javascript" src="http://www.' + catalog + '.catalogi.ru/static/js/kmoquery.min.js"></script>'
+                '<script type="text/javascript" src="http://www.' + catalog + '.catalogi.ru/static/js/kmoquery.min.js"></script>',
+                '<script type="text/javascript" src="http://www.' + catalog + '.catalogi.ru/static/js/Product.min.js"></script>'
             ].join("\n"),
         }
     }
