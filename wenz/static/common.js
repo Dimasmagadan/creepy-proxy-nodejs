9f31cac9df482d6af1d2f1f628e5d15353d132ef
Alexander Tretiak <p.seredin@proffibit.com>
2016-05-10 12:37:45 +0300

    login

--- wenz/static/common.js
+++ wenz/static/common.js
@@ -76,7 +76,7 @@ catalogi.parse = function() {
         .css('color', 'white')
         .attr('onclick', '')
         .attr('href', 'http://catalogi.ru/katalog_wenz/');
-
+    catalogi('.categoryNavESpot a').attr('href', 'http://catalogi.ru/katalog_wenz/');
 
     catalogi("#iframe").hide();
     catalogi('.leafletHeaderIcon').remove();
