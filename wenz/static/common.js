ec35f7047e3a0ba40088e017c07949978db8e49f
Alexander Tretiak <p.seredin@proffibit.com>
2016-05-10 12:09:33 +0300

    header btns

--- wenz/static/common.js
+++ wenz/static/common.js
@@ -22,7 +22,9 @@ catalogi.noTranslate = function() {
     catalogi('span:contains("Wohnen")').text('Декор');
     catalogi('span[title="SALE"]').addClass('notranslate');
     catalogi('.accountHeaderIcon').addClass('notranslate');
+    catalogi('.accountHeaderIcon .headerIconText')
     catalogi('#miniShopCart').addClass('notranslate');
+    catalogi('#miniShopCart .headerIconText').text('Корзина');
     catalogi('#categoryNavigation > div > ul > li.last.categoryNavESpot > div > p > a').attr('href', '');
     // Список
     //console.log(catalogi('title').text() + ' ' + catalogi('title').text().search(/marken/i));
