c629cd52fe8ca73dc044e3c513f5a6e88d5a2d1b
alexandertretiak <a.symonenkov@proffibit.com>
2016-05-13 12:34:40 +0300

    footer

--- sheego/static/common.js
+++ sheego/static/common.js
@@ -146,6 +146,17 @@ catalogi.parse = function() {
         .delay(5000)
         .queue(function(next) {
 
+
+    $( "#header-topbar > div > div" ).append( "<div id='topbar-slogan' class='hidden-xs hidden-sm col-md-6'><a href='http://www.catalogi.ru' target='_blank' class='headerLinks _home catalogiLink'> Каталоги.ру - заказ и доставка одежды из интернет-магазина Sheego.de.</a></div>"
+                                            + "<div class='hidden-xs hidden-sm col-md-6'>"
+                                            + "<a href='#' class='headerLinks' onclick='catalogi.catalogs(); return false' style='margin-left:0px; margin-right:0px;'>Онлайн каталоги</a>|"
+                                            + "<a href='#' class='headerLinks' onclick='catalogi.shops(); return false' style='margin-left:0px; margin-right:0px;'>Интернет-магазины</a>|"
+                                            + "<a href='#' class='headerLinks' onclick='catalogi.sizeTable(); return false' style='margin-left:0px; margin-right:0px;'>Таблица размеров</a>|"
+                                            + "<a href='#' class='headerLinks' onclick='catalogi.delivery(); return false' style='margin-left:0px; margin-right:0px;'>Доставка</a>|"
+                                            + "<a href='#' class='headerLinks' onclick='catalogi.payment(); return false' style='margin-left:0px; margin-right:0px;'>Оплата</a></div>" );
+                                        
+
+
             if (_auth) {
                 catalogi('#_auth_wait').remove();
                 catalogi('.myaccount.notranslate > a').remove();
@@ -172,20 +183,11 @@ catalogi.parse = function() {
 
         //Added by Artem Symonenkov 12.05.2016
 
-    //custom header     
+
+                //custom header     
     $('#topbar-slogan').remove();
     $('#topbar-salutation').remove();
     $('#topbar-bulletpoints').remove();
-
-    $( "#header-topbar > div > div" ).append( "<div id='topbar-slogan' class='hidden-xs hidden-sm col-md-6'><a href='http://www.catalogi.ru' target='_blank' class='headerLinks _home catalogiLink'> Каталоги.ру - заказ и доставка одежды из интернет-магазина Sheego.de.</a></div>"
-                                            + "<div class='hidden-xs hidden-sm col-md-6'>"
-                                            + "<a href='#' class='headerLinks' onclick='catalogi.catalogs(); return false' style='margin-left:0px; margin-right:0px;'>Онлайн каталоги</a>|"
-                                            + "<a href='#' class='headerLinks' onclick='catalogi.shops(); return false' style='margin-left:0px; margin-right:0px;'>Интернет-магазины</a>|"
-                                            + "<a href='#' class='headerLinks' onclick='catalogi.sizeTable(); return false' style='margin-left:0px; margin-right:0px;'>Таблица размеров</a>|"
-                                            + "<a href='#' class='headerLinks' onclick='catalogi.delivery(); return false' style='margin-left:0px; margin-right:0px;'>Доставка</a>|"
-                                            + "<a href='#' class='headerLinks' onclick='catalogi.payment(); return false' style='margin-left:0px; margin-right:0px;'>Оплата</a></div>" );
-                                        
-
 
     //hide all content after first #footer
     $("#footer").nextAll().remove();
