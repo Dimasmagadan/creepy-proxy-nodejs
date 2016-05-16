2f87867b33cb1be2cae514980257fe9f9f08f8ac
alexandertretiak <a.symonenkov@proffibit.com>
2016-05-16 14:40:57 +0300

    color and size

--- sheego/static/common.js
+++ sheego/static/common.js
@@ -292,7 +292,7 @@ function checkBasket() {
     $('#minibasket').empty();
     $('#minibasket').attr("onclick","catalogi.order(); return false");
         $('#minibasket').append(""
-                    + "<a class='minibasket__link js-minibasket-href' >"
+                    + "<a class='minibasket__link js-minibasket-href' style='cursor : pointer'>"
                     +    "<span class='minibasket__text hidden-xs-inline hidden-sm-inline'>"   
                     +    "<span class='minibasket__basket'>Корзина</span>"
                     +    "</span>"
