38dd5decb6c4982dbe5eed233d76d9cfc98ab70c
Artem symonenkov <a.symonenkov@proffibit.com>
2016-05-17 17:07:05 +0300

    cart

--- bonaparte/static/anthem.js
+++ bonaparte/static/anthem.js
@@ -1015,11 +1015,10 @@ function Anthem_CallBack(url, target, id, method, args, clientCallBack, clientCa
 		    }
 		    return result;
 	    }
-	    var action1 = Anthem_GetCallBackUrl();
-	    var action = action1.replace("www.bonaparte.catalogi.ru","de.bonaparteshop.com");
+	    var action = Anthem_GetCallBackUrl();
+	    //var action1 = action.replace("www.bonaparte.catalogi.ru","de.bonaparteshop.com");
 	    x.open("POST", url ? url : action);
 	    x.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=utf-8");
-	    x.setRequestHeader("Origin","https://de.bonaparteshop.com");
 	    x.setRequestHeader("Accept-Encoding", "gzip, deflate");
 	    if (typeof(clientCallBack) == "function") {
 		    x.onreadystatechange = function() {
