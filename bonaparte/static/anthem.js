79248bec7a282d5affd6c1a973b9963f4f8713fb
Artem symonenkov <a.symonenkov@proffibit.com>
2016-05-17 16:35:33 +0300

    cart

--- bonaparte/static/anthem.js
+++ bonaparte/static/anthem.js
@@ -1020,8 +1020,8 @@ function Anthem_CallBack(url, target, id, method, args, clientCallBack, clientCa
 	    x.open("POST", url ? url : action);
 	    x.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=utf-8");
 	    x.setRequestHeader('Access-Control-Allow-Origin', '*');
-	    x.setRequestHeader('Access-Control-Allow-Headers: Content-Type, Content-Range, Content-Disposition, Content-Description');
-	    x.setRequestHeader('Access-Control-Max-Age: 1728000');
+	    x.setRequestHeader('Access-Control-Allow-Headers', 'Content-Description');
+	    x.setRequestHeader('Access-Control-Max-Age', '1728000');
 	    //x.setRequestHeader("Accept-Encoding", "gzip, deflate");
 	    if (typeof(clientCallBack) == "function") {
 		    x.onreadystatechange = function() {
